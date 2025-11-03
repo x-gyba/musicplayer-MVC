5afa6ea25bc0ec1674a7b256a7a52b16f61f5032
Clayton Campos <30420230+x-gyba@users.noreply.github.com>
2025-11-03 17:11:28 -0300

    Delete loja-MVC directory

--- loja-MVC/layouts/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-    <div class="footer-content">
-        <p>&copy; <?php echo date('Y'); ?> Cestas Online. Todos os direitos reservados.</p>
-    </div>
-</footer>
--- loja-MVC/views/loja/home.php
+++
@@ -1,3 +0,0 @@
-<!-- views/loja/home.php -->
-<h2>Bem-vindo!</h2>
-<p>Esta é a página inicial da loja.</p>
